fed8fa39d4eda20c34d54ecd160e77bb0a6e3688
Kazz <6817525+kazeed@users.noreply.github.com>
2021-04-22 17:10:35 +0200

    Remove unused ctor

--- swplus/swplus.api/Models/Character.cs
+++ swplus/swplus.api/Models/Character.cs
@@ -4,40 +4,6 @@ namespace swplus.api.Models
 {
     public class Character
     {
-        public Character(string name, int height, string homeworld, string gender, string specie)
-        {
-            if (string.IsNullOrEmpty(name))
-            {
-                throw new ArgumentException($"'{nameof(name)}' cannot be null or empty.", nameof(name));
-            }
-
-            if (string.IsNullOrEmpty(homeworld))
-            {
-                throw new ArgumentException($"'{nameof(homeworld)}' cannot be null or empty.", nameof(homeworld));
-            }
-
-            if (string.IsNullOrEmpty(gender))
-            {
-                throw new ArgumentException($"'{nameof(gender)}' cannot be null or empty.", nameof(gender));
-            }
-
-            if (string.IsNullOrEmpty(specie))
-            {
-                throw new ArgumentException($"'{nameof(specie)}' cannot be null or empty.", nameof(specie));
-            }
-
-            Name = name;
-            Height = height;
-            Homeworld = homeworld;
-            Gender = gender;
-            Specie = specie;
-
-            // Auto props
-            Id = Guid.NewGuid();
-            Created = DateTime.UtcNow;
-            Edited = DateTime.UtcNow;
-        }
-
         public Character(Character basis, Character other)
         {
             Id = basis.Id;
